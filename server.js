f828bb416ace024b52111b93c5891a98b6d2639d
felix24mihai <mihai_prava@yahoo.com>
2022-03-21 23:22:21 +0200

    Add user model

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API Running..'));
 
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profiles', require('./routes/api/profiles'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}..`));
