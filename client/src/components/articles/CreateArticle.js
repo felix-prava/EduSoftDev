4070a12fcb18dc76585c27a652941253b3d79772
felix24mihai <mihai_prava@yahoo.com>
2022-06-22 23:52:54 +0300

    Go back

--- client/src/components/articles/CreateArticle.js
+++ client/src/components/articles/CreateArticle.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addArticle } from '../../actions/article';
@@ -10,7 +11,119 @@ const CreateArticle = ({ addArticle }) => {
     body: '',
   });
 
-  return <Fragment>CreateArticle</Fragment>;
+  const { subject, body, description } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Fragment>
+      <div className='container mt-8'>
+        <form
+          className='space-y-8 divide-y divide-gray-200'
+          onSubmit={(e) => onSubmit(e)}
+        >
+          <div className='space-y-8 divide-y divide-gray-200'>
+            <div>
+              <div>
+                <h3 className='text-2xl font-bold leading-6 font-medium text-gray-900 sm:text-2xl'>
+                  Create an article
+                </h3>
+              </div>
+
+              <div className='mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6'>
+                <div className='sm:col-span-6'>
+                  <label
+                    htmlFor='subject'
+                    className='block text-sm font-medium text-gray-700'
+                  >
+                    {' '}
+                    What's this article about?{' '}
+                  </label>
+                  <div className='mt-1'>
+                    <input
+                      type='text'
+                      name='subject'
+                      value={subject}
+                      onChange={(e) => onChange(e)}
+                      className='shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md'
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className='mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6'>
+                <div className='sm:col-span-6'>
+                  <label
+                    htmlFor='body'
+                    className='block text-sm font-medium text-gray-700'
+                  >
+                    {' '}
+                    Body{' '}
+                  </label>
+                  <div className='mt-1'>
+                    <textarea
+                      name='body'
+                      value={body}
+                      onChange={(e) => onChange(e)}
+                      rows='3'
+                      className='shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border border-gray-300 rounded-md'
+                    ></textarea>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <div className='mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6'>
+                <div className='sm:col-span-3'>
+                  <label
+                    htmlFor='description'
+                    className='block text-sm font-medium text-gray-700'
+                  >
+                    {' '}
+                    Short description{' '}
+                  </label>
+                  <div className='mt-1'>
+                    <input
+                      type='text'
+                      name='description'
+                      value={description}
+                      onChange={(e) => onChange(e)}
+                      className='shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md'
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='pt-5'>
+            <div className='flex justify-end mb-8'>
+              <Link to='/my-profile'>
+                <button
+                  type='button'
+                  className='bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-offset-2 focus:ring-indigo-500'
+                >
+                  Cancel
+                </button>
+              </Link>
+              <button
+                type='submit'
+                className='ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-offset-2 focus:ring-indigo-500'
+              >
+                Save
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </Fragment>
+  );
 };
 
 CreateArticle.propTypes = {
