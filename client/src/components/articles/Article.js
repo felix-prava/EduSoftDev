3a0ab6b7c1032b93055db09d62093e379ffaa2c7
felix24mihai <mihai_prava@yahoo.com>
2022-07-06 21:47:16 +0300

    Leave a comment to an article

--- client/src/components/articles/Article.js
+++ client/src/components/articles/Article.js
@@ -3,6 +3,7 @@ import { Link, useParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import AddComment from './AddComment';
 import {
   getArticle,
   addLike,
@@ -55,98 +56,102 @@ const Article = ({
             {article.subject}
           </h2>
           <p className='mt-10 text-base text-gray-500'>{article.body}</p>
-        </div>
 
-        <div className='float-right'>
-          <div className='mt-6 flex space-x-3 '>
-            <button
-              type='button'
-              onClick={(e) => likeArticle(article._id)}
-              className='inline-flex items-center px-4 py-1 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500'
-            >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='h-5 w-5'
-                viewBox='0 0 20 20'
-                fill='currentColor'
+          <div className='mr-8 float-right'>
+            <div className='mt-6 flex space-x-3 '>
+              <button
+                type='button'
+                onClick={(e) => likeArticle(article._id)}
+                className='inline-flex items-center px-4 py-1 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500'
               >
-                <path d='M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z' />
-              </svg>
-              {article.likes.length > 0 && article.likes.length}
-            </button>
-            <button
-              type='button'
-              onClick={(e) => dislikeArticle(article._id)}
-              className='inline-flex items-center px-4 py-1 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500'
-            >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='h-5 w-5 mt-2'
-                viewBox='0 0 20 20'
-                fill='currentColor'
-              >
-                <path d='M18 9.5a1.5 1.5 0 11-3 0v-6a1.5 1.5 0 013 0v6zM14 9.667v-5.43a2 2 0 00-1.105-1.79l-.05-.025A4 4 0 0011.055 2H5.64a2 2 0 00-1.962 1.608l-1.2 6A2 2 0 004.44 12H8v4a2 2 0 002 2 1 1 0 001-1v-.667a4 4 0 01.8-2.4l1.4-1.866a4 4 0 00.8-2.4z' />
-              </svg>
-              {article.dislikes.length > 0 && article.dislikes.length}
-            </button>
-
-            {!auth.loading &&
-              auth.user !== null &&
-              auth.user._id === article.user && (
-                <button
-                  onClick={(e) => deleteArticle(article._id)}
-                  className='bg-red-600 border border-transparent rounded-md shadow-sm py-1 px-3 flex items-center inline-flex justify-center ml-4 text-sm font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-600'
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  className='h-5 w-5'
+                  viewBox='0 0 20 20'
+                  fill='currentColor'
                 >
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    className='h-5 w-5'
-                    viewBox='0 0 20 20'
-                    fill='currentColor'
-                  >
-                    <path
-                      fillRule='evenodd'
-                      d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z'
-                      clipRule='evenodd'
-                    />
-                  </svg>
-                </button>
-              )}
-          </div>
+                  <path d='M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z' />
+                </svg>
+                {article.likes.length > 0 && article.likes.length}
+              </button>
+              <button
+                type='button'
+                onClick={(e) => dislikeArticle(article._id)}
+                className='inline-flex items-center px-4 py-1 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500'
+              >
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  className='h-5 w-5 mt-2'
+                  viewBox='0 0 20 20'
+                  fill='currentColor'
+                >
+                  <path d='M18 9.5a1.5 1.5 0 11-3 0v-6a1.5 1.5 0 013 0v6zM14 9.667v-5.43a2 2 0 00-1.105-1.79l-.05-.025A4 4 0 0011.055 2H5.64a2 2 0 00-1.962 1.608l-1.2 6A2 2 0 004.44 12H8v4a2 2 0 002 2 1 1 0 001-1v-.667a4 4 0 01.8-2.4l1.4-1.866a4 4 0 00.8-2.4z' />
+                </svg>
+                {article.dislikes.length > 0 && article.dislikes.length}
+              </button>
 
-          <div className='mt-6 flex items-center '>
-            <div className='flex-shrink-0'>
-              <span className='sr-only'>
-                {article.userLastName} {article.userFirstName}
-              </span>
-              <img
-                className='h-10 w-10 rounded-full'
-                src={article.avatar}
-                alt=''
-              />
-            </div>
-            <div className='ml-3'>
-              <p className='text-sm font-medium text-gray-900'>
-                {article.user ? (
-                  <Link to={`/profiles/${article.user}`}>
-                    {article.userLastName} {article.userFirstName}
-                  </Link>
-                ) : (
-                  <Fragment>
-                    {article.userLastName} {article.userFirstName}
-                  </Fragment>
+              {!auth.loading &&
+                auth.user !== null &&
+                auth.user._id === article.user && (
+                  <button
+                    onClick={(e) => deleteArticle(article._id)}
+                    className='bg-red-600 border border-transparent rounded-md shadow-sm py-1 px-3 flex items-center inline-flex justify-center ml-4 text-sm font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-600'
+                  >
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      className='h-5 w-5'
+                      viewBox='0 0 20 20'
+                      fill='currentColor'
+                    >
+                      <path
+                        fillRule='evenodd'
+                        d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z'
+                        clipRule='evenodd'
+                      />
+                    </svg>
+                  </button>
                 )}
-              </p>
-              <div className='flex space-x-1 text-sm text-gray-500'>
-                <time dateTime={article.date}>
-                  {' '}
-                  Posted on <Moment format='DD/MM/YYYY'>
-                    {article.date}
-                  </Moment>{' '}
-                </time>
+            </div>
+
+            <div className='mt-6 flex items-center '>
+              <div className='flex-shrink-0'>
+                <span className='sr-only'>
+                  {article.userLastName} {article.userFirstName}
+                </span>
+                <img
+                  className='h-10 w-10 rounded-full'
+                  src={article.avatar}
+                  alt=''
+                />
+              </div>
+              <div className='ml-3'>
+                <p className='text-sm font-medium text-gray-900'>
+                  {article.user ? (
+                    <Link to={`/profiles/${article.user}`}>
+                      {article.userLastName} {article.userFirstName}
+                    </Link>
+                  ) : (
+                    <Fragment>
+                      {article.userLastName} {article.userFirstName}
+                    </Fragment>
+                  )}
+                </p>
+                <div className='flex space-x-1 text-sm text-gray-500'>
+                  <time dateTime={article.date}>
+                    {' '}
+                    Posted on{' '}
+                    <Moment format='DD/MM/YYYY'>{article.date}</Moment>{' '}
+                  </time>
+                </div>
               </div>
             </div>
           </div>
         </div>
+        {auth.isAuthenticated && (
+          <div className='mt-8 pt-2'>
+            <AddComment articleId={article._id} />
+          </div>
+        )}
       </div>
     </Fragment>
   );
--- client/src/components/articles/CreateArticle.js
+++ client/src/components/articles/CreateArticle.js
@@ -108,7 +108,7 @@ const CreateArticle = ({ addArticle }) => {
 
           <div className='pt-5'>
             <div className='flex justify-end mb-8'>
-              <Link to='/my-profile'>
+              <Link to='/articles'>
                 <button
                   type='button'
                   className='bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-offset-2 focus:ring-indigo-500'
