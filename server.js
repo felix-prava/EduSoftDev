d740364d2b91d30e236079a87a2a34dc89770681
felix24mihai <mihai_prava@yahoo.com>
2023-03-13 21:11:28 +0200

    Update API port

--- server.js
+++ server.js
@@ -30,6 +30,6 @@ app.use(
 );
 app.use('/api/learning-materials', require('./routes/api/learningMaterials'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3200;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}..`));
