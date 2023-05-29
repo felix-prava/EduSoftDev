7961647b3e8838aef87b6365fcc29779776cb565
felix24mihai <mihai_prava@yahoo.com>
2023-05-29 11:56:59 +0300

    Modify server.js for heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('./path');
 
 const app = express();
 
@@ -8,8 +9,6 @@ connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
-
-app.get('/', (_, res) => res.send('API Running..'));
 
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
@@ -30,6 +29,15 @@ app.use(
 );
 app.use('/api/solutions', require('./routes/api/solutions'));
 app.use('/api/learning-materials', require('./routes/api/learningMaterials'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 3200;
 
