99c1e7ec2b34347b5d6e74417b74317714615799
felix24mihai <mihai_prava@yahoo.com>
2023-06-09 12:02:41 +0300

    Add first image to codebase

--- client/src/components/basics-of-programming/MaterialItem.js
+++ client/src/components/basics-of-programming/MaterialItem.js
@@ -53,7 +53,7 @@ const MaterialItem = ({
         <div className='flex-shrink-0'>
           <img
             className='h-48 w-full object-cover'
-            src='https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80'
+            src={process.env.PUBLIC_URL + '/images/lessonImage.jpeg'}
             alt=''
           />
         </div>
