b62bd0241dd7dc304be31d83c861ce8c993d4740
felix24mihai <mihai_prava@yahoo.com>
2023-06-24 14:17:37 +0300

    Hide solving button if user do not have enough experience points

--- client/src/components/basics-of-programming/Quiz.js
+++ client/src/components/basics-of-programming/Quiz.js
@@ -132,20 +132,29 @@ const Quiz = ({
                   {backButtonLabel}
                 </button>
               </Link>
-              <button
-                type='button'
-                onClick={() => {
-                  if (userAnswers.length === 0) {
-                    setAlert(`${selectOneOptionLabel}`, 'error', 4500, true);
-                    return;
-                  }
-                  solveQuiz(quizId, userAnswers);
-                  unselectUserAnswers(answers);
-                }}
-                className='ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-offset-2 focus:ring-indigo-500'
-              >
-                {finishQuizLabel}
-              </button>
+              {user &&
+                learningMaterial &&
+                user.exp >= learningMaterial.expNeeded && (
+                  <button
+                    type='button'
+                    onClick={() => {
+                      if (userAnswers.length === 0) {
+                        setAlert(
+                          `${selectOneOptionLabel}`,
+                          'error',
+                          4500,
+                          true
+                        );
+                        return;
+                      }
+                      solveQuiz(quizId, userAnswers);
+                      unselectUserAnswers(answers);
+                    }}
+                    className='ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-offset-2 focus:ring-indigo-500'
+                  >
+                    {finishQuizLabel}
+                  </button>
+                )}
             </div>
           </div>
         </form>
